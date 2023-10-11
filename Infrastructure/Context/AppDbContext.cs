6d6cf8712ec2d71779d7ff90e2cdd62db91623fa
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-11 16:05:12 -0300

    adicionando contexto

---
+++ Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context
+{
+    public class AppDbContext : DbContext
+    {
+        AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+    
+}
