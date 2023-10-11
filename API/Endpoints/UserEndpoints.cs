89cfdf5efcd0ddbf2d49e55599784b3d59b05fe5
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-11 16:43:47 -0300

    adicionando rota que cria usuario

---
+++ API/Endpoints/UserEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using Application.Services;
+
+namespace API.Endpoints
+{
+    public static class UserEndpoints
+    {
+        public static void MapUserEndpoints(this IEndpointRouteBuilder app) 
+        {
+            app.MapPost("api/users", async (CreateUserRequest request, UserService userService) =>
+            {
+                var userId = await userService.CreateAsync(
+                    request.Name,
+                    request.Email);
+
+                return Results.Ok(userId);
+            });
+        }
+    }
+}
