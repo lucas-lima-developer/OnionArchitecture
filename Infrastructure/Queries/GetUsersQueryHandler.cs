29043bb3d5971f5161816b128ca2f4652eb3a871
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-11 16:20:55 -0300

    adicionando implementação da query que retorna todos os usuários

---
+++ Infrastructure/Queries/GetUsersQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.Queries;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Queries
+{
+    public class GetUsersQueryHandler : IGetUsersQueryHandler
+    {
+        private readonly AppDbContext _context;
+
+        public GetUsersQueryHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<UserResponse>> Handle()
+        {
+            var users = await _context.Users.AsNoTracking()
+                .Select(user => new UserResponse
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Email = user.Email
+                })
+                .ToListAsync();
+
+            return users;
+        }
+    }
+}
