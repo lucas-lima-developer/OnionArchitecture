55621025b9cd5fb7f46a05bfe8e9583b258ec29f
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-11 16:13:06 -0300

    adicionando implementação da query que busca usuário pelo id

---
+++ Infrastructure/Queries/GetUserByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Queries;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Queries
+{
+    public class GetUserByIdQueryHandler : IGetUserByIdQueryHandler
+    {
+        private readonly AppDbContext _context;
+
+        public GetUserByIdQueryHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserResponse?> Handler(Guid id)
+        {
+            var userResponse = await _context
+                .Users
+                .AsNoTracking()
+                .Where(user => user.Id == id)
+                .Select(user => new UserResponse
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Email = user.Email
+                })
+                .FirstOrDefaultAsync();
+
+            return userResponse;
+        }
+    }
+}
